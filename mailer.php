c8cbfc91c9a1903e2cdeafe47ea292ff9c29b025
Petr Fila <pet.fila@gmail.com>
2018-06-28 02:29:53 +1000

    message form works

--- mailer.php
+++ mailer.php
@@ -27,10 +27,10 @@ try {
     //Server settings
     $mail->SMTPDebug = 4;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+    $mail->Host = 'smtp.email.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'myanotheremail83@gmail.com'; //getenv('EMAIL');                 // SMTP username
-    $mail->Password = 'testingaccount!)@('; //getenv('PASS');                           // SMTP password
+    $mail->Username = 'youraddress@email.com'; //getenv('EMAIL');                 // SMTP username
+    $mail->Password = 'password'; //getenv('PASS');                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
