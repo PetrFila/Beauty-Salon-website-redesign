84d93299939bb60925f622adf6012fd52260c23d
Petr Fila <pet.fila@gmail.com>
2018-06-28 02:06:28 +1000

    changing email credentials

--- mailer.php
+++ mailer.php
@@ -6,21 +6,31 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
+// require __DIR__.'/vendor/autoload.php';
 
 $name = $_POST['name']; // required
 $email_from = $_POST['email']; // required
 $message = $_POST['message']; // required
+$envFile = '/.env';
+// $dotenv = new Dotenv\Dotenv(dirname($envFile));
 
+if (file_exists($envFile)) {
+    (new Dotenv\Dotenv(dirname($envFile)))->load();
+}
+// $dotenv = new Dotenv\Dotenv($envFile);
+// $dotenv->load();
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
+
+    
     //Server settings
     $mail->SMTPDebug = 4;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'pet.fila@gmail.com';                 // SMTP username
-    $mail->Password = 'filkus79';                           // SMTP password
+    $mail->Username = 'myanotheremail83@gmail.com'; //getenv('EMAIL');                 // SMTP username
+    $mail->Password = 'testingaccount!)@('; //getenv('PASS');                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
